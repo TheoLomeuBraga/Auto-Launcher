e5176eb2696b8a329d0cbad7da5c78b3101b8830
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2024-08-06 08:10:31 -0300

    a

---
+++ assets/player/player.gd
@@ -0,0 +1,17 @@
+extends BasicMovement
+
+func look_around(delta):
+	rotation_degrees.y += (mouse_movement.x * delta) + (Input.get_axis("look_left","ui_right") * Global.joystick_sensitivity * delta)
+	$Camera3D.rotation_degrees.x += (mouse_movement.y * delta) + (Input.get_axis("look_up","look_down") * Global.joystick_sensitivity * delta)
+	if $Camera3D.rotation_degrees.x > 90:
+		$Camera3D.rotation_degrees.x = 90
+	if $Camera3D.rotation_degrees.x < -90:
+		$Camera3D.rotation_degrees.x = -90
+
+@export var jump_power := 6.0
+
+func movement_plugin(delta):
+	move_direction = (basis.z.normalized() * Input.get_axis("walk_foward","walk_back")) + (basis.x.normalized() * Input.get_axis("walk_left","walk_right") )
+	look_around(delta)
+	if in_floor and Input.is_action_just_pressed("jump"):
+		jump(jump_power)
