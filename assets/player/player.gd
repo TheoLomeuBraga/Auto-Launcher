b68fd5d434662b0777cde15807637c1e0ea46657
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2024-08-07 08:51:42 -0300

    a

--- assets/player/player.gd
+++ assets/player/player.gd
@@ -1,12 +1,12 @@
 extends BasicMovement
 
-@export var reverseCannon : NodePath
-var reverseCannonNode : Node3D
+@export var gunHand : NodePath
+var gunHandNode : Node3D
 
 func _ready():
-	reverseCannonNode = get_node(reverseCannon)
-	if reverseCannonNode != null:
-		reverseCannonNode.player = self
+	gunHandNode = get_node(gunHand)
+	if gunHandNode != null and  gunHandNode.get_child(0) != null:
+		gunHandNode.get_child(0).player = self
 
 func look_around(delta):
 	
@@ -15,8 +15,8 @@ func look_around(delta):
 	else:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	
-	rotation_degrees.y += (mouse_movement.x * delta) + (Input.get_axis("look_left","ui_right") * Global.joystick_sensitivity * delta)
-	$Camera3D.rotation_degrees.x += (mouse_movement.y * delta) + (Input.get_axis("look_up","look_down") * Global.joystick_sensitivity * delta)
+	rotation_degrees.y += (mouse_movement.x * delta) + (Input.get_axis("look_right","look_left") * Global.joystick_sensitivity * delta * 90)
+	$Camera3D.rotation_degrees.x += (mouse_movement.y * delta) + (Input.get_axis("look_down","look_up") * Global.joystick_sensitivity * delta * 90)
 	if $Camera3D.rotation_degrees.x > 90:
 		$Camera3D.rotation_degrees.x = 90
 	if $Camera3D.rotation_degrees.x < -90:
@@ -27,10 +27,14 @@ func look_around(delta):
 
 
 func movement_plugin(delta):
-	if reverseCannonNode != null and Input.is_action_just_pressed("shot"):
-		reverseCannonNode.shot()
+	if gunHandNode != null and  gunHandNode.get_child(0) != null and Input.is_action_just_pressed("shot"):
+		gunHandNode.get_child(0).shot()
 	
 	move_direction = (basis.z.normalized() * Input.get_axis("walk_foward","walk_back")) + (basis.x.normalized() * Input.get_axis("walk_left","walk_right") )
 	look_around(delta)
 	if in_floor and Input.is_action_just_pressed("jump"):
 		jump(jump_power)
+	
+	$Label.text = "ms: " + str(int(linear_velocity.length()))
+	
+	
