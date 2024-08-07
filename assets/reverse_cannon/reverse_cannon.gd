1c5bf811e3c1f4e88be8344fc51ab3414f13fbed
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2024-08-06 22:07:42 -0300

    a

--- assets/reverse_cannon/reverse_cannon.gd
+++ assets/reverse_cannon/reverse_cannon.gd
@@ -3,6 +3,8 @@ extends BaseGun
 var player : RigidBody3D
 
 @export var power = 10
+@export var max_speed = 50
+
 func shot():
 	player.sliding_time = 0.5
-	player.linear_velocity = global_basis.x * (player.linear_velocity.length() + power)
+	player.linear_velocity = global_basis.x * (min(max_speed,player.linear_velocity.length()) + power)
