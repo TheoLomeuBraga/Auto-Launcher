47814acac6d8bd340859a0ec04781f3686aaa1f1
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2024-08-15 16:56:25 -0300

    a

--- assets/end_door/end_door.gd
+++ assets/end_door/end_door.gd
@@ -1,9 +1,10 @@
 extends Node3D
 
-@export var open := true
+var open := true
 @export var next_sceane = ""
 
 #door scale 8x8
+@export var aways_open : bool = false
 var open_state : float = 0
 func _ready():
 	if Global.chalange_time_left > 0:
@@ -18,6 +19,8 @@ func _process(delta):
 		open_state += delta
 	open_state = max(0,min(1,open_state))
 	$end_door/entity0_brush14.position.z = open_state * 60
+	if aways_open:
+		$end_door/entity0_brush14.position.z = 60
 
 
 func _on_area_3d_body_entered(body):
