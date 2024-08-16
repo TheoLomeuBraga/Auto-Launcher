567fe13fa417474dfde2e8c620206a7b13c5293a
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2024-08-16 09:23:36 -0300

    a

--- assets/player/player.gd
+++ assets/player/player.gd
@@ -15,6 +15,14 @@ func _ready():
 	
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
+
+var mouse_movement = Vector2.ZERO
+func _input(event):
+	if event is InputEventMouseMotion:
+		mouse_movement = -event.relative * Global.mouse_sensitivity
+	else:
+		mouse_movement = Vector2.ZERO
+
 func look_around(delta):
 	
 	
